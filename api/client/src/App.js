bccfb39cd589b9779980a5049834040a75d94ede
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-08-12 13:43:55 +0930

    added basic deaders

---
+++ api/client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+        <div className="App">
+            <h1>Welcome, PK</h1>
+            <h4>Your Tasks</h4>
+        </div>
+    );
+
+    }
+
+    export default App;
