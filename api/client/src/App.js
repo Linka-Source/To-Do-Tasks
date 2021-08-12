ee7e57b8ac662633f2ec32edce55432d52460791
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-08-12 13:52:04 +0930

    added elements for todos

--- api/client/src/App.js
+++ api/client/src/App.js
@@ -3,7 +3,18 @@ function App() {
         <div className="App">
             <h1>Welcome, PK</h1>
             <h4>Your Tasks</h4>
+
+            <div className="todos">
+                 <div className="todo">
+                     <div className="checkbox"></div>
+
+            <div className="text">Go shopping</div>
+
+            <div className="delete-todo">x</div>
+
+            </div>
         </div>
+    </div>
     );
 
     }
